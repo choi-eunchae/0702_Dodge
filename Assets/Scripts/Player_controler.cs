9790be87bf371f49d62bd24d24da7ccca6801025
DESKTOP-JB52TBT\202_20 <chaecha20@naver.com>
2025-07-02 11:24:38 +0900

    브런치 변경 후 두 번째 커밋

--- Assets/Scripts/Player_controler.cs
+++ Assets/Scripts/Player_controler.cs
@@ -32,29 +32,29 @@ public class Player_controler : MonoBehaviour
         playerRigidbody.linearVelocity = newVelocity;
 
 
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            // Log a message to the console when the up arrow key is pressed
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
+        // if (Input.GetKey(KeyCode.UpArrow) == true)
+        // {
+        //     // Log a message to the console when the up arrow key is pressed
+        //     playerRigidbody.AddForce(0f, 0f, speed);
+        // }
 
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            // Log a message to the console when the down arrow key is pressed
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
+        // if (Input.GetKey(KeyCode.DownArrow) == true)
+        // {
+        //     // Log a message to the console when the down arrow key is pressed
+        //     playerRigidbody.AddForce(0f, 0f, -speed);
+        // }
 
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            // Log a message to the console when the right arrow key is pressed
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
+        // if (Input.GetKey(KeyCode.RightArrow) == true)
+        // {
+        //     // Log a message to the console when the right arrow key is pressed
+        //     playerRigidbody.AddForce(speed, 0f, 0f);
+        // }
 
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            // Log a message to the console when the left arrow key is pressed
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        // if (Input.GetKey(KeyCode.LeftArrow) == true)
+        // {
+        //     // Log a message to the console when the left arrow key is pressed
+        //     playerRigidbody.AddForce(-speed, 0f, 0f);
+        // }
 
     }
 }
