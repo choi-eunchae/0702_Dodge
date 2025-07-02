70b76ffe91e8d6d417b218a39553aeb14771316e
DESKTOP-JB52TBT\202_20 <chaecha20@naver.com>
2025-07-02 11:18:01 +0900

    controler 변경

--- Assets/Scripts/Player_controler.cs
+++ Assets/Scripts/Player_controler.cs
@@ -6,17 +6,32 @@ using UnityEngine;
 
 public class Player_controler : MonoBehaviour
 {
-    public Rigidbody playerRigidbody; 
-    public float speed = 8f; 
+    private Rigidbody playerRigidbody; 
+    public float speed = 8f;
 
     void Start()
     {
-
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        //수평과 수직 축 입력 값을 감지하여 저장
+        float xinput = Input.GetAxis("Horizontal");
+        Debug.Log("xinput: " + xinput);
+        float zinput = Input.GetAxis("Vertical");
+        Debug.Log("zinput: " + zinput);
+
+       //실제 이동 속도를 입력 값과 이동 속력을 통해 결정
+        float xSpeed = xinput * speed;
+        float zSpeed = zinput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed); 
+
+        playerRigidbody.linearVelocity = newVelocity;
+
+
         if (Input.GetKey(KeyCode.UpArrow) == true)
         {
             // Log a message to the console when the up arrow key is pressed
