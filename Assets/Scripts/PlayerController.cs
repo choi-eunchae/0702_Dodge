1a52740372778fb89a4b2850d884412d192c4013
DESKTOP-JB52TBT\202_20 <chaecha20@naver.com>
2025-07-03 09:43:54 +0900

    Asset 스토어 LowPoly무료로 받음

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
-using UnityEditor.Build.Content;
+//using JetBrains.Annotations;
+//using UnityEditor.Build.Content;
 using UnityEngine;
 
 
